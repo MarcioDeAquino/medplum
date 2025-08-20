700e8a2b6be023197d5575f45e8c256accb167b1
Márcio de Aquino <18746739+MarcioDeAquino@users.noreply.github.com>
2025-08-20 11:52:19 -0300

    remove test.setup.ts from src/examples and adjust vite.config.ts

--- examples/medplum-demo-bots/vite.config.ts
+++ examples/medplum-demo-bots/vite.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test.setup.ts',
   },
 });
